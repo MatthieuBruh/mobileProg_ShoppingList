5d1fd62e5ac98996736c7f8a0575548ead8d5e6c
MatthieuBruh <matthieu.bruhwiler@etu.hesge.ch>
2022-09-10 18:18:10 +0300

    Shopping List completed

--- App.js
+++ App.js
@@ -1,10 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, FlatList, Button } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [item, setItem] = React.useState('');
+  const [shoppingList, setShoppingList] = React.useState([]);
+
+  const addItem = () => {
+    if (item != '') {
+      setShoppingList([...shoppingList, { key: item, value: item }]);
+      setItem('');
+      console.log(shoppingList);
+    }
+  }
+
+  const clear = () => {
+    setShoppingList([]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <TextInput
+        style={styles.textInputStyle}
+        onChangeText={value => setItem(value)}
+        value={item}
+      />
+      <View style={{ flexDirection: "row", justifyContent: 'center', alignItems: 'center', width: '75%' }}>
+        <Button
+            style={styles.buttonStyle}
+            title="Add"
+            onPress={addItem}
+        />
+
+        <Button
+            style={styles.buttonStyle}
+            title="Clear"
+            onPress={clear}
+        />
+      </View>
+      
+
+      <View style={{ flexDirection: "column", justifyContent: 'center', alignItems: 'center', width: '75%', margin: 20 }}>
+        <Text style= {{ color: 'blue', fontSize: 16, fontWeight: 'bold' }} >Shopping List</Text>
+        <FlatList
+            data={shoppingList}
+            renderItem={({item}) =><Text>{item.key}</Text>}
+            keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +62,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonStyle: {
+    width: 10,
+    height: 10,
+    margin: 20,
+    alignContent: 'center',
+  },
+  textInputStyle: {
+    width: 250,
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    margin:20
+  }
 });
